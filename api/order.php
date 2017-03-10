4648c36196f5fe3796cf578a52ac5c4dff217f60
LockDownRU <kirill300300@gmail.com>
2017-03-10 19:20:28 +0300

    HTML Mail

--- api/order.php
+++ api/order.php
@@ -1,21 +1,40 @@
 <?php
-$product = $_GET['product'];
-$fio = $_GET['fio'];
-$email = $_GET['email'];
-$phone = $_GET['phone'];
-$address = $_GET['address'];
-$note = $_GET['note'];
+$product = htmlentities($_GET['product']);
+$fio = htmlentities($_GET['fio']);
+$email = htmlentities($_GET['email']);
+$phone = htmlentities($_GET['phone']);
 
-//ilya.silitski@gmail.com
-$mail = 'ilya.silitski@gmail.com';
-http_response_code(200);
+$locality = htmlentities($_GET['locality']);
+$street = htmlentities($_GET['street']);
+$house = htmlentities($_GET['house']);
+$housing = htmlentities($_GET['housing']);
+$apartment = htmlentities($_GET['apartment']);
 
-exit(0);
+$note = htmlentities($_GET['note']);
+
+// ilya.silitski@gmail.com kirill300300@gmail.com
+$to = 'ilya.silitski@gmail.com';
+
+$subject = "Заказ: $product";
+
+$headers = "From: xn80ab6c@vh93.hoster.by\r\n";
+$headers .= "Reply-To: xn80ab6c@vh93.hoster.by\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
 $message = "
-    Товар: $product\n
-    ФИО: $fio\n
-    Email: $email\n
-    Телефон: $phone\n
-    Адрес: $address\n
-    Комментарий к заказу: $note\n";
-mail($mail, "Заказ: $product", $message);
+    Товар: <u>$product</u><br>
+    Фамилия, Имя: $fio<br>
+    Email: $email<br>
+    Телефон: $phone<br>
+    Адрес:<br>
+    - Населенный пункт: $locality<br>
+    - Улица: $street<br>
+    - Дом: $house<br>
+    - Корпус: $housing<br>
+    - Дом: $apartment<br>
+    Комментарий к заказу: $note";
+
+mail($to, $subject, $message, $headers);
+
+http_response_code(200);
